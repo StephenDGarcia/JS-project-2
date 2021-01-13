502ddf596796281e18c7739589f5baaebfa5ded1
StephenDGarcia <stephendgarcia@gmail.com>
2021-01-13 08:36:19 -0600

    pagination button
    halfway through pagination button. working on for loop and linkList.insertAdjacentElement

--- js/script.js
+++ js/script.js
@@ -36,15 +36,15 @@ function showPage(list, page) {
       let studentItem = list[i];
       console.log(studentItem);
       
-      studentItem.insertAdjacentHTML( beforeend, `
+      studentList.insertAdjacentHTML( 'beforeend', `
       <li class="student-item cf">
       <div class="student-details">
-        <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
-        <h3>${list.name}</h3>
-        <span class="email">ethel.dean@example.com</span>
+        <img class="avatar" src=${studentItem.picture.thumbnail} alt="Profile Picture">
+        <h3>${studentItem.name.first}</h3>
+        <span class="email">${studentItem.email}</span>
       </div>
       <div class="joined-details">
-        <span class="date">Joined 12-15-2005</span>
+        <span class="date">${studentItem.registered.date}</span>
       </div>
     </li>
       `)
@@ -61,7 +61,33 @@ showPage(data, 1);
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
+function addPagination(list) {
+   // create a variable to calculate the number of pages needed
+   let numOfPages = Math.ceil( list.length / 9 )
+  // select the element with a class of `link-list` and assign it to a variable
+   let linkList = document.querySelector('.link-list')
+  // set the innerHTML property of the variable you just created to an empty string
+   linkList.innerHTML = '';
+  // loop over the number of pages needed
+   for (let i = 1; i <= numOfPages; i++) {
+    // create the elements needed to display the pagination button
+    (let button = `
+      <li>
+         <button type="button">1</button>
+      </li>
+      `);
+    // insert the above elements
+    linkList.insertAdjacentElement('beforeend', 'button');
+   }
+  // give the first pagination button a class of "active"
 
-
+  // create an event listener on the `link-list` element
+    // if the click target is a button:
+      // remove the "active" class from the previous button
+      // add the active class to the clicked button
+      // call the showPage function passing the `list` parameter and page to display as arguments
+   console.log(list);
+}
+ addPagination(data);
 
 // Call functions
