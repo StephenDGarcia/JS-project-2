379ecbc2624dd4b64652c5f96fd86691032e151e
StephenDGarcia <stephendgarcia@gmail.com>
2021-01-17 20:22:51 -0600

    Finally working
    Could be cleaned up a little with variables set on top but it's finally doing what I need to do.

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
+const itemsPerPage = 9;
 
 
 /*
@@ -17,24 +18,23 @@ This function will create and insert/append the elements needed to display a "pa
 */
 function showPage(list, page) {
    //Start Index = (page parameter * items per page) - items per page
-   let startIndex = (page * 9) - 8;
+   let startIndex = (page * itemsPerPage) - itemsPerPage;
    //End Index = page parameter * items per page
-      let endIndex = page * 9;
+   let endIndex = page * itemsPerPage;
       //Select the UL element with a class of student-list and assign its value to a variable.
    let studentList = document.querySelector('.student-list');
    //Use the innerHTML property set the HTML content of the student-list variable you just created to an empty string. This will remove any students that might have previously been displayed.
    studentList.innerHTML = '';
    //Loop over the list parameter.
-   for ( let i = 0; i < list.length; i++ ) {
+   for ( let i = 1; i < list.length; i++ ) {
    //Inside the loop:
       //Write a conditional statement that checks if the current index (i) is greater than or equal to the start index variable and less than the end index variable.
-      if ( i >= startIndex && i <= endIndex) {
+      if ( i > startIndex && i <= endIndex) {
       //Inside that conditional:
       //Create the DOM elements needed to display the information for each matching student as you iterate over the list parameter. Here is an example of what the the final version of these elements should look like:
          //Pro Tip: Because you will need to create multiple elements to display the information for each student, you might consider using a template literal for this
       //Insert the elements you have created to the student-list variable you created earlier. The insertAdjacentHTML method and beforeend option works well for this.
       let studentItem = list[i];
-     // console.log(studentItem);
       
       studentList.insertAdjacentHTML( 'beforeend', `
       <li class="student-item cf">
@@ -50,10 +50,6 @@ function showPage(list, page) {
       `)
       }
    }
- 
- //console.log(startIndex);
- //console.log(endIndex);
-
 };
 
 showPage(data, 1);
@@ -63,38 +59,39 @@ This function will create and insert/append the elements needed for the paginati
 */
 function addPagination(list) {
    // create a variable to calculate the number of pages needed
-   let numOfPages = Math.ceil( list.length / 9 );
+   let numOfPages = Math.ceil( list.length / itemsPerPage );
    console.log(numOfPages);
   // select the element with a class of `link-list` and assign it to a variable
-   let linkList = document.querySelector('.link-list')
-  // set the innerHTML property of the variable you just created to an empty string
-   linkList.innerHTML = '';
+  const linkList = document.querySelector('.link-list');
+  // set the innerHTML property of the variable you just created to an empty string 
+  linkList.innerHTML = '';
   // loop over the number of pages needed
+  let button = '';
    for (let i = 1; i <= numOfPages; i++) {
     // create the elements needed to display the pagination button
-    let button = `
+    button = `
       <li>
          <button type="button">${i}</button>
       </li>
       `;
     // insert the above elements
     linkList.insertAdjacentHTML('beforeend', button);
-   }
+   };
+   
+   let activeButton = linkList.querySelector('button');   
   // give the first pagination button a class of "active"
-   document.querySelector('button').className = 'active';
+   activeButton.className = 'active';
+   console.log(activeButton);
   // create an event listener on the `link-list` element
-     linkList.addEventListener('click', (e) => {
-   //    // if the click target is a button:
-       clickedButton = e.target.tagName;
-       if ( clickedButton === 'BUTTON') {
-   //    // remove the "active" class from the previous button
-       active = document.querySelector('.active');
-       active = '';
-   //    // add the active class to the clicked button
-       clickedButton.className = 'active';
-       console.log(clickedButton);
-   //    // call the showPage function passing the `list` parameter and page to display as arguments
-      showPage(list, button)
+   linkList.addEventListener('click', (e) => {
+   // if the click target is a button:
+      if ( e.target.tagName === 'BUTTON') {
+         // remove the "active" class from the previous button
+         linkList.querySelector('.active').className = '';
+         // add the active class to the clicked button
+         e.target.className = 'active';
+       // call the showPage function passing the `list` parameter and page to display as arguments
+      showPage(list, e.target.textContent)
        };
     });
   };  //console.log(list);
